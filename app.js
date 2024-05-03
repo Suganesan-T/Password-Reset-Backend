280223f7488012ae0a0c30a2fe32ee37b7d0da59
Suganesan-T <suganesan71@gmail.com>
2024-05-03 12:04:18 +0530

    orign updated

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://cerulean-pastelito-5561d7.netlify.app');
+    res.header('Access-Control-Allow-Origin', 'https://cerulean-pastelito-5561d7.netlify.app/');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
